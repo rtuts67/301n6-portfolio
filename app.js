583180d1fdd6572db925e72617ec9ee5b378c3e2
Rosemary Tuttle <rosemary.brinton@gmail.com>
2017-02-13 19:48:39 -0800

    did loadAll function

--- app.js
+++ app.js
@@ -1,48 +1,28 @@
-'use strict;'
-var allProjects = [];
-//\var articleView = {};
-function MyProject (proj) {
-  this.name = proj.name;
-  this.path = proj.path;
-  this.description = proj.description;
-  this.languages = proj.languages;
-}
+'use strict';
 
-MyProject.prototype.toHtml = function() {
-  var htmlTemplate = $('#template').html();
-  var template = Handlebars.compile(htmlTemplate);
-  return template(this);
-};
+(function(module) {
 
-myLocalProjects.forEach(function(currentArticleObj) {
-  allProjects.push(new MyProject(currentArticleObj));
-});
+  function MyProject (proj) {
+    this.name = proj.name;
+    this.filepath = proj.path;
+    this.description = proj.description;
+    this.languages = proj.languages;
+  }
 
-allProjects.forEach(function(project) {
-  $('#Projects').append(project.toHtml());
-});
+  MyProject.allProjects = [];
 
+  MyProject.prototype.toHtml = function() {
+    var htmlTemplate = $('#template').html();
+    var template = Handlebars.compile(htmlTemplate);
+    return template(this);
+  };
 
-//articleView.populateli = function() {
-//  $('article').not('.template').each(function() {
-//    var name, description, languages, ptag;
-//    name = $(this).find('name').text();
-//    description = $(this).find('description').text();
-  //  languages = $(this).find('languages').text();
-//    ptag = '<p>' + name + description + languages + '</p>';
-  //  $('#myprojects').append(ptag);
-//  });
-//}
-//articleView.populateli();
-
-//new MyProject('Rosemary Tuttle Guessing Game', 'https://rtuts67.github.io/guessing_game/', 'This was the very first project I worked on as an aspiring developer.', lang)
-//new MyProject('Campfire Coffee', 'https://rtuts67.github.io/lab7/', 'This second project was heavily weigted on JavaScript.', lang)
-//new MyProject('Bus Mall', 'https://rtuts67.github.io/lab11/', 'The third project involved localStorage!', lang)
-//new MyProject('Home Field Advantage', 'https://rtuts67.github.io/Final_Project_HFA_201n6/', 'The final project was built for the purpose of helping prospective home-buyers look for a property in Seattle.', lang)
-//TODO:
-//create prototype method that includes .toHTML and renders projects to the domain with jquery
-//add nav bar
-//create event listener for nav bar,
-//add data-attributes to show certain sections based on nav bar
-//create function to show what is selected in nav bar
-//use jquery to hide what i dont need
+  MyProject.loadAll = function(proj) {
+    proj.forEach(function(currentProj) {
+      MyProject.allProjects.push(new MyProject(currentProj));
+    });
+  }
+  MyProject.loadAll();
+  //MyProject.allProjects.forEach(function(project) {
+    //$('#Projects').append(project.toHtml());
+})(window);
