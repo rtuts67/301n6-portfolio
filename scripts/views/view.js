456d883440c1a51730394435d2f7c1526f2afe96
Rosemary Tuttle <rosemary.brinton@gmail.com>
2017-04-27 19:38:54 -0700

    changing view.js

--- scripts/views/view.js
+++ scripts/views/view.js
@@ -1,12 +1,12 @@
 'use strict';
-var handleTheNavMenue = function() {
-  $('#visible').show();
-  $('.navigation').on('click', '.navigation-item', function(e) {
-    $('.tab-content').hide();
-    $(`#${$(this).data('content')}`).fadeIn(400);
-  });
-  $('.navigation .navigation-item:first').click();
-};
+// var handleTheNavMenue = function() {
+//   $('#visible').show();
+//   $('.navigation').on('click', '.navigation-item', function(e) {
+//     $('.tab-content').hide();
+//     $(`#${$(this).data('content')}`).fadeIn(400);
+//   });
+//   $('.navigation .navigation-item:first').click();
+// };
 
 var showProjects = function () {
   MyProject.allProjects.forEach(function(proj) {
