a6f5c05c16ad9c306d538502839f4f5dd9c4b3f0
Rosemary Tuttle <rosemary.brinton@gmail.com>
2017-05-17 23:49:16 -0700

    why is page not defined

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-var express = require('express'),
-  app = express(),
-  port = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000;
 
-app.get('/', function(req, res) {
+app.use(express.static('./'));
+
+function callBack(req, res) {
   res.sendFile('index.html', {root: '.'});
-})
+}
 
-app.use(express.static('.'));
+app.get('*', callBack)
 
 app.get('*', function(req, res) {
   res.send('404! Routes not working.');
 })
 
-app.listen(port, () => console.log('Listening on port' + port));
+app.listen(PORT, () => console.log('Listening on port' + PORT));
