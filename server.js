d8c68708792857c6256469f781ed3485587de29a
Rosemary Tuttle <rosemary.brinton@gmail.com>
2017-05-17 20:48:41 -0700

    made change on server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express'),
   port = process.env.PORT || 3000;
 
 app.use(express.static('./'));
-app.get('/', function(req, res) {
+app.get('*', function(req, res) {
   res.sendFile('index.html', {root: '.'});
 })
 app.listen(port, () => console.log('Listening on port' + port));
